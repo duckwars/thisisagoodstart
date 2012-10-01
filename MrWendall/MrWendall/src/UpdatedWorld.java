b39a01618a8984df084a551fa6c20aa29705b79f
duckwars <obamaforyomama@gmail.com>
2012-10-01 01:54:38 -0700

    Gave it a better name, made it do some maths

---
+++ MrWendall/MrWendall/src/UpdatedWorld.java
@@ -0,0 +1,13 @@
+public class UpdatedWorld {
+	public static void main(String[] args) {
+		int x = 6;
+		int y = 15;
+		int z = (x + y);
+		int aa = (x * y);
+		System.out.println("All Your Base Are Belong To Us!");
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(aa);
+	}
+}
