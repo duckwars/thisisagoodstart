76c0f36e4a572a7c23bbcf6c291a4a182bb41868
kymoon <kymoon@github>
2012-11-08 21:51:56 -0800

    OK now it does something.

--- MrWendall/MrWendall/src/ScannerExample.java
+++ MrWendall/MrWendall/src/ScannerExample.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /**
  * This doesn't do anything yet.
  */
@@ -6,6 +8,23 @@ public class ScannerExample {
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        System.out.println("Keep on typing stuff and hitting enter");
+
+        final Scanner scanner = new Scanner(System.in);
+        try {
+            while (scanner.hasNext()) {
+                final String token = scanner.next();
+
+                doSomethingWithToken(token);
+            }
+        } finally {
+            // Normally good practice to close() in a finally block, but you
+            // probably don't want to close System.in.
+            // scanner.close();
+        }
+    }
+
+    private static void doSomethingWithToken(final String token) {
+        System.out.println("SARCASM: " + token);
     }
 }
