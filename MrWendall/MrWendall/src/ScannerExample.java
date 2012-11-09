c3ae592dff2bdd5e9c58939aefe201e849dd66c8
kymoon <kymoon@github>
2012-11-08 21:45:40 -0800

    This is something I pushed.

---
+++ MrWendall/MrWendall/src/ScannerExample.java
@@ -0,0 +1,11 @@
+/**
+ * This doesn't do anything yet.
+ */
+public class ScannerExample {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+    }
+}
