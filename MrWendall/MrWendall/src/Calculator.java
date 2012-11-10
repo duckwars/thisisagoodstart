1ffdc8e6f9b88477a559478d060e633ab48cda54
duckwars <obamaforyomama@gmail.com>
2012-11-10 02:23:41 -0800

    calculator bothers me.

--- MrWendall/MrWendall/src/Calculator.java
+++ MrWendall/MrWendall/src/Calculator.java
@@ -3,48 +3,82 @@ import java.util.Scanner;
 public class Calculator {
 	
 	public static void main(String args[]) {
-		boolean calculate = true;
-			
-		while (calculate == true) {
-		Scanner in1 = new Scanner(System.in);
-		int fnum, snum;
-		fnum = in1.nextInt();
-		Scanner operation = new Scanner(System.in);
-		String math = operation.nextLine();
-		Scanner in2 = new Scanner(System.in);
-		snum = in2.nextInt();
 		
-		if(math.equals("*") ) {
-			int total = fnum * snum;
-			System.out.println(total);
-	
-		}
-		if(math.equals("/") ) {
-			int total = fnum / snum;
-			System.out.println(total);
-		}
-			if(math.equals("+") ) {
-				int total = fnum + snum;
-				System.out.println(total);
-			}
-			
-			if(math.equals("-") ) {
-				int total = fnum - snum;
-				System.out.println(total);
-			}
-			
-			System.out.println("Would you like to go again?");
-			Scanner again = new Scanner(System.in);
-			// It looks as though it gets up to here, and then after taking input it goes ape shit
-				if(again.equals("no")) {
-					calculate = false;
-				}else {
-					calculate = true;
-				}
-			
-		}
-			System.out.println("Thanks Buddy!");
+		System.out.println("Welcome to the calculator!");
+		Scanner scanner = new Scanner(System.in);
 		
+		int fnum = scanner.nextInt();
+		String operator = scanner.next();
+		int snum = scanner.nextInt();;
+		int total;
+		
+		
+		if ("+".equals(operator)) {
+			System.out.println(operator);
+			total = fnum + snum;
+		} 
+		if ("-".equals(operator)) {
+			System.out.println(operator);
+			total = fnum - snum;
+		}
+		if ("*".equals(operator)) {
+			System.out.println(operator);
+			total = fnum * snum;
+		}
+		if ("/".equals(operator)) {
+			System.out.println(operator);
+			total = fnum / snum;
+		}
+		else {
+			System.out.println("I don't understand... sorry!");
+			total = 0;
+		}
+		
+	   System.out.println(total);
+		
+//		System.out.println(fnum+" " +operator+" " +snum);
+		
+/*		switch (operator) {
+		
+		case "+":
+			System.out.println("It's a plus!");
+//			int total = fnum + snum;
+			break;
+		
+		case "-":
+			System.out.println("It's a minus!");
+//			total = fnum - snum;
+			break;
+		
+		case "*":
+			System.out.println("It's a multiple!");
+//			total = fnum * snum;
+			break;
+		
+		case "/":
+			System.out.println("It's a division!");
+//			total = fnum / snum;
+			break;
+		
+
+		}
+********////////		
+		
+//		System.out.println(total);
+//		System.out.print(fnum + " ");
+//		System.out.print(operation);
+//		System.out.print(snum);
+//		System.out.println(fnum);
+//		System.out.println(operation);
+//		System.out.println(snum);
+		
+
 	}
+
+
+
+//private static void doCalculation(final String token) {
+ //   System.out.println("SARCASM: " + token);
+//}
 
 }
